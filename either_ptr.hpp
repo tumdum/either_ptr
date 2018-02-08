705780002de5484b8ecd62284994518e21401f23
Tomasz Kłak <tomasz@tomaszklak.pl>
2018-02-08 18:35:00 +0100

    Initial commit.

---
+++ either_ptr.hpp
@@ -0,0 +1,89 @@
+#ifndef EITHER_PTR_HPP_
+#define EITHER_PTR_HPP_
+
+#include <cstdint>
+#include <cassert>
+
+namespace tmd
+{
+
+template <typename L, typename R>
+struct default_deallocator
+{
+    void left(L* l) const { delete l; }
+    void right(R* r) const { delete r; }
+};
+
+template <typename L, typename R, typename D = default_deallocator<L, R>>
+struct either_ptr
+{
+    using Left = L;
+    using Right = R;
+    using Self = either_ptr<Left, Right, D>;
+
+    static_assert(alignof(L) > 1, "");
+    static_assert(alignof(R) > 1, "");
+
+    static Self left(L* l)
+    { 
+        return Self{(void*)l}; 
+    }
+
+    static Self right(R* r)
+    { 
+        Self self{(void*)r};
+        self.m_data.num |= 1UL;
+        return self;
+    }
+
+    template<typename F>
+    auto visit(const F& f) const -> decltype(f.left(static_cast<L*>(nullptr)))
+    {
+        if (isLeft())
+        {
+            return f.left(reinterpret_cast<Left*>(m_data.num));
+        }
+        else
+        {
+            return f.right(reinterpret_cast<Right*>(m_data.num & ~1UL));
+        }
+    }
+
+    either_ptr(const either_ptr&) = default;
+    either_ptr& operator=(const either_ptr&) = default;
+
+    either_ptr(either_ptr&&) = default;
+    either_ptr& operator=(either_ptr&&) = default;
+
+    ~either_ptr()
+    {
+        visit(D{});
+    }
+
+private:
+    either_ptr(void* p)
+    {
+        /* 
+         * This assert is not useless, consider what will happen when L or T is
+         * set to this type:
+         * using AlignedChar = const char __attribute__((aligned (2)));
+         * All static asserts will be satisfied yet it will be still possible to
+         * pass character literals which can be allocated at addresses that have
+         * lowest bit set.
+         */
+        m_data.ptr = p;
+        assert(isLeft());
+    }
+
+    bool isLeft() const { return (m_data.num & 1) == 0; }
+
+    union
+    {
+        void* ptr;
+        uintptr_t num;
+    } m_data;
+};
+
+}
+
+#endif  // EITHER_PTR_HPP_
---
+++ main.cc
@@ -0,0 +1,82 @@
+#include "either_ptr.hpp"
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <type_traits>
+
+struct StdStringer 
+{
+    template <typename T>
+    std::string left(const T* p) const { return std::to_string(*p); }
+    template <typename T>
+    std::string right(const T* p) const { return std::to_string(*p); }
+
+    std::string left(const std::string* p) const { return *p; }
+    std::string right(const std::string* p) const { return *p; }
+};
+
+struct Extractor
+{
+    template <typename T>
+    T left(const T* p) const { return *p; }
+    template <typename T>
+    T right(const T* p) const { return *p; }
+};
+
+template <int M>
+struct Mult
+{
+    template <typename T>
+    void left(T* p) const { *p *= M; }
+    template <typename T>
+    void right(T* p) const { *p *= M; }
+
+    void left(std::string* p) const { *p = *p + " * " + std::to_string(M); }
+    void right(std::string* p) const { left(p); }
+};
+
+struct RightHeapDeleter
+{
+    template <typename T>
+    void left(T* ) const { }
+    template <typename T>
+    void right(T* p) const { delete p; }
+};
+
+template <typename T>
+T to(const int i) { return i; }
+template <>
+std::string to<std::string>(const int i) { return std::to_string(i); }
+
+template <typename T>
+void test()
+{
+    using TPtr = tmd::either_ptr<T, T, RightHeapDeleter>;
+    static_assert(sizeof(TPtr) == sizeof(T*), "");
+    T i = to<T>(10);
+    T* p = new T{to<T>(42)};
+    const auto s1 = TPtr::left(&i);
+    const auto s2 = TPtr::right(p);
+
+    assert(i == s1.visit(Extractor{}));
+    assert(*p == s2.visit(Extractor{}));
+
+    std::cout << "s1: " << s1.visit(StdStringer{}) << std::endl;
+    std::cout << "s2: " << s2.visit(StdStringer{}) << std::endl;
+
+    s1.visit(Mult<10>{});
+    s2.visit(Mult<10>{});
+
+    assert(i == s1.visit(Extractor{}));
+    assert(*p == s2.visit(Extractor{}));
+
+    std::cout << "s1: " << s1.visit(StdStringer{}) << std::endl;
+    std::cout << "s2: " << s2.visit(StdStringer{}) << std::endl;
+}
+
+int main() {
+    test<short>();
+    test<int>();
+    test<double>();
+    test<std::string>();
+}
